68586d8f94744f2a02fe6aa5dbbafc9ae6beb965
Ventsislav Kisyov <ventsislav755@gmail.com>
2024-03-26 23:24:11 +0200

    added configurations

---
+++ telerikAcademyProject/src/main/java/com/telerikAcademy/telerikAcademyProject/Configuration/HibernateConfig.java
@@ -0,0 +1,50 @@
+package com.telerikAcademy.telerikAcademyProject.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateConfig {
+    private String dbUrl;
+    private String dbUsername;
+    private String dbPassword;
+
+    public HibernateConfig(Environment env){
+        dbUrl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dbPassword = env.getProperty("database.password");
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.telerikAcademy.telerikAcademyProject.Models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource= new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL");
+        //hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        return  hibernateProperties;
+    }
+}
