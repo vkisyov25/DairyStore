f2f99ced8ab87750b8d82b76b128f8a59ce8da61
vkisyov25 <ventsislav755@gmail.com>
2025-01-05 20:34:23 +0200

    code refactoring

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Utils/JwtUtil.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Utils/JwtUtil.java
@@ -7,17 +7,6 @@ import java.util.Date;
 @Component
 public class JwtUtil {
     private static final String SECRET_KEY = "2e73012638c9f832c35c3f45e3b2e257b9204f85ea13a1a0bdbf2542e45bc6f3";
-    private static final long EXPIRATION_TIME = 86400000; // 1 ден
-
-    /*public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }*/
-
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
@@ -27,19 +16,13 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
-    /*public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
-            return true;
-        } catch (JwtException e) {
-            return false;
-        }
-    }*/
+
     // Метод за валидиране на токен
     public boolean validateToken(String token) {
         String username = extractUsername(token);
         return (username != null && !isTokenExpired(token));
     }
+
     // Проверка за изтекъл токен
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
@@ -51,7 +34,6 @@ public class JwtUtil {
                 .getBody()
                 .getExpiration();
     }
-
     public String extractUsername(String token) {
         return Jwts.parser()
                 .setSigningKey(SECRET_KEY)
