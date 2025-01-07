a21b9151284e2e9185dfcc2d07b1f6e0ac04ebe8
vkisyov25 <ventsislav755@gmail.com>
2025-01-07 23:22:23 +0200

    Add @NonNull annotations to doFilterInternal method parameters

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Configurations/JwtAuthenticationFilter.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Configurations/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -26,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         this.userDetailsService = userDetailsService;
     }
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         String token = null;
 
         // Извличаме токена от HTTP-only cookie
