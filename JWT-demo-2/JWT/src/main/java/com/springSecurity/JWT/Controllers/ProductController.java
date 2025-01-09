30c17f45a53ef8cf2325ffe4146db8a432f2d1e6
vkisyov25 <ventsislav755@gmail.com>
2025-01-09 22:15:19 +0200

    added functionality in ProductController for creation product for sale

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/ProductController.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.springSecurity.JWT.Controllers;
 
 import com.springSecurity.JWT.Models.Product;
+import com.springSecurity.JWT.Models.User;
+import com.springSecurity.JWT.Repository.UserRepository;
+import com.springSecurity.JWT.Security.CustomUserDetailsService;
 import com.springSecurity.JWT.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,24 +14,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
+    private final CustomUserDetailsService customUserDetailsService;
     @Autowired
-    public ProductController(ProductService productService) {
+    private UserRepository userRepository;
+
+    @Autowired
+    public ProductController(ProductService productService, CustomUserDetailsService customUserDetailsService) {
         this.productService = productService;
+        this.customUserDetailsService = customUserDetailsService;
     }
-    @PreAuthorize("hasAuthority('seller')")
+
+    //@PreAuthorize("hasAuthority('seller')")
     @GetMapping("/all")
-    public String getAllProductsByUsername(Model model){
+    public String getAllProductsByUsername(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
             String username = authentication.getName();
@@ -39,6 +47,21 @@ public class ProductController {
         }
         //TODO: Не може да я достъпим ако не сме логнати
         return "login"; // Ако няма логнат потребител
+    }
+
+    @GetMapping("/create")
+    public String showCreateProductPage(Model model) {
+        model.addAttribute("product", new Product());
+        return "createProductPage";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@ModelAttribute Product product) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByUsername(authentication.getName());
+        product.setUser(user);
+        productService.creteProduct(product);
+        return "sellerPage";
     }
 
 }
