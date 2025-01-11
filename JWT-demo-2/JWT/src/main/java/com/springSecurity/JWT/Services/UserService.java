77937c9edf35fb0f67028d1b70eca567b5169773
vkisyov25 <ventsislav755@gmail.com>
2025-01-11 23:32:21 +0200

    created UserServices

---
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/UserService.java
@@ -0,0 +1,19 @@
+package com.springSecurity.JWT.Services;
+
+import com.springSecurity.JWT.Models.User;
+import com.springSecurity.JWT.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
