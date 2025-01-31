6e2e3a882148f8ca6dd7fd2c15fff41897a5d2bf
vkisyov25 <ventsislav755@gmail.com>
2025-01-31 08:37:30 +0200

    added validation for password strong

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Models/User.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Models/User.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+
 import java.util.List;
 
 @ValidSellerFields
@@ -28,6 +29,7 @@ public class User {
     private String username;
 
     @NotBlank(message = "Password cannot be blank")
+    @Pattern(regexp = "^([A-Za-z@.!%0-9]{8,}+)$", message = "The password must be at least 8 characters long and contain at least: one uppercase letter, " + "one lowercase letter, one number, and one special character (@, $, !, %, , ?, &).")
     private String password;
 
     @NotBlank
