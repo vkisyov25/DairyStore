091229ae5d6a4a3f8ef9724c720aaaa292fbb180
Ventsislav Kisyov <92498007+vkisyov25@users.noreply.github.com>
2025-02-09 18:32:04 +0200

    Create dto for user registration (#1)

---
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Models/dtos/CreateUserDto.java
@@ -0,0 +1,45 @@
+package com.springSecurity.JWT.Models.dtos;
+
+import com.springSecurity.JWT.Models.Cart;
+import com.springSecurity.JWT.Models.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.List;
+
+public class CreateUserDto {
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    //@Pattern(regexp = "^([A-Za-z@.!%0-9]{8,}+)$", message = "The password must be at least 8 characters long and contain at least: one uppercase letter, " + "one lowercase letter, one number, and one special character (@, $, !, %, , ?, &).")
+    private String password;
+
+    @NotBlank
+    private String authorities;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotBlank(message = "Phone number cannot be blank.")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be exactly 10 digits.")
+    private String phone;
+
+    @NotBlank(message = "Address cannot be blank")
+    private String address;
+
+    //@Column(unique = true) //Махнах ги заради проблема при регистрация на потребители с роля byuer защото като
+    // companyName и companyEIK са празни и в базата данни има потребител с празни полета не може да се запази в базата данни новия потребител
+    private String companyName;
+
+    //@Column(unique = true) // както при companyName
+    //@Pattern(regexp = "^[0-9]{9}$")
+    private String companyEIK;
+
+}
