87b9bd9a258a144bfd76da1c8a071050f014425c
vkisyov25 <ventsislav755@gmail.com>
2025-02-11 17:51:07 +0200

    updated ProductService

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/ProductService.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/ProductService.java
@@ -1,34 +1,54 @@
 package com.springSecurity.JWT.Services;
 
 import com.springSecurity.JWT.Models.Product;
+import com.springSecurity.JWT.Models.User;
 import com.springSecurity.JWT.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+    private final UserService userService;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, UserService userService) {
         this.productRepository = productRepository;
+        this.userService = userService;
     }
 
-    public List<Product> getAllProductByUsername(String username){
-       return productRepository.findProductsByUsername(username);
-    }
-    public Product createProduct(Product product){
-        return productRepository.save(product);
-    }
-
-    public List<Product> getProductsByType(String productType){
-        return productRepository.findProductsByType(productType);
+    public List<Product> getAllProductByUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            return productRepository.findProductsByUsername(username);
+        }
+        return new ArrayList<>();
     }
 
-    public List<Product> getAllProducts(){
-        return productRepository.findAll();
+    public void createProduct(Product product) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new SecurityException("User is not authenticated");
+        }
+        User user = userService.getUserByUsername(authentication.getName());
+        product.setUser(user);
+        productRepository.save(product);
     }
+
+    public List<Product> getProductsByType(String productType) {
+        if (productType.equals("всички")) {
+            return productRepository.findAll();
+        } else {
+            return productRepository.findProductsByType(productType);
+        }
+    }
+
     public Product getProductById(Long productId) {
         return productRepository.findProductById(productId);
     }
