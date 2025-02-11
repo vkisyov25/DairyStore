b48844b689012f911291a12672c8c898e86c472d
Ventsislav Kisyov <92498007+vkisyov25@users.noreply.github.com>
2025-02-11 20:41:35 +0200

    refactored productController
    * updated ProductController
    
    * updated ProductService
    
    * updated listToBuyPage

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/ProductController.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/ProductController.java
@@ -1,22 +1,11 @@
 package com.springSecurity.JWT.Controllers;
 
 import com.springSecurity.JWT.Models.Product;
-import com.springSecurity.JWT.Models.User;
-import com.springSecurity.JWT.Repository.UserRepository;
-import com.springSecurity.JWT.Security.CustomUserDetailsService;
 import com.springSecurity.JWT.Services.ProductService;
-import com.springSecurity.JWT.Services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,62 +13,44 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
-    private final UserService userService;
 
-    public ProductController(ProductService productService, UserService userService) {
+    public ProductController(ProductService productService) {
         this.productService = productService;
-        this.userService = userService;
     }
 
-    //@PreAuthorize("hasAuthority('seller')")
     @GetMapping("/all")
-    public String getAllProductsByUsername(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            String role = authentication.getAuthorities().iterator().next().getAuthority();
-            List<Product> products = productService.getAllProductByUsername(username);
-            model.addAttribute("productList", products);
-            return "listOfProduct";
-        }
-        //TODO: Не може да я достъпим ако не сме логнати
-        return "login"; // Ако няма логнат потребител
+    public String getProductsByUsername(Model model) {
+        List<Product> products = productService.getAllProductByUsername();
+        model.addAttribute("productList", products);
+        return "listOfProduct";
+
     }
 
     @GetMapping("/create")
-    public String showCreateProductPage(Model model) {
+    public String displayCreateProductPage(Model model) {
         model.addAttribute("product", new Product());
         return "createProductPage";
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createProduct(@RequestBody Product product) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.getUserByUsername(authentication.getName());
-        product.setUser(user);
+    public ResponseEntity<?> createNewProduct(@RequestBody Product product) {
         productService.createProduct(product);
         return ResponseEntity.ok("Product created successfully!");
     }
    /* @PostMapping("/create")
     public String createProduct(@ModelAttribute Product product) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.getUserByUsername(authentication.getName());
-        product.setUser(user);
         productService.creteProduct(product);
         return "sellerPage";
     }*/
 
     @GetMapping("/listToBuy")
-    public String showListToBuy(){
+    public String displayListToBuyPage() {
         return "listToBuyPage";
     }
-    @PostMapping("/listToBuy")
-    public String viewProductsListToBuy(@RequestParam String productType, Model model){
-        if (productType.equals("всички")){
-            model.addAttribute("productList", productService.getAllProducts());
-        }else {
-            model.addAttribute("productList", productService.getProductsByType(productType));
-        }
+
+    @GetMapping("/listToBuy")
+    public String displayProductsByType(@RequestParam String productType, Model model) {
+        model.addAttribute("productList", productService.getProductsByType(productType));
         return "listToBuyPage";
     }
 
