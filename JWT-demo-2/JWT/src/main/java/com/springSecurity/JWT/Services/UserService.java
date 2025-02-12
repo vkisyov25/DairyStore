b5ae865d5b6756d08329d963821b7bf1c1c5775f
vkisyov25 <ventsislav755@gmail.com>
2025-02-12 16:50:49 +0200

    updated UserService

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/UserService.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/UserService.java
@@ -3,17 +3,25 @@ package com.springSecurity.JWT.Services;
 import com.springSecurity.JWT.Models.User;
 import com.springSecurity.JWT.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User getUserByUsername(String username){
-        return userRepository.findByUsername(username);
+    public User getUserByUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = null;
+        if (authentication != null && authentication.isAuthenticated()) {
+            user = userRepository.findByUsername(authentication.getName());
+        }
+        return user;
     }
 }
