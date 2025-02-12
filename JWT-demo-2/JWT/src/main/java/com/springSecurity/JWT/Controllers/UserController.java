4013333f26c633b4303af2bec98e94d1da8e2795
Ventsislav Kisyov <92498007+vkisyov25@users.noreply.github.com>
2025-02-12 17:01:16 +0200

    Refactor/user controller (#5)
    * updated UserController
    
    * updated UserService
    
    * updated logic on getUserByUsername method
    
    * updated logic on getUserByUsername

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/UserController.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/UserController.java
@@ -1,32 +1,25 @@
 package com.springSecurity.JWT.Controllers;
 
-import com.springSecurity.JWT.Models.User;
 import com.springSecurity.JWT.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/allInformation")
-    public String getUserByUsername(Model model){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = null;
-        if(authentication !=null && authentication.isAuthenticated()){
-            user = userService.getUserByUsername(authentication.getName());
-        }
-        model.addAttribute("user", user);
+    public String getUserByUsername(Model model) {
+        model.addAttribute("user", userService.getUserByUsername());
         return "userInformationPage";
     }
 }
--- JWT-demo-2/JWT/src/test/java/com/springSecurity/JWT/Services/ProductServiceTest.java
+++ JWT-demo-2/JWT/src/test/java/com/springSecurity/JWT/Services/ProductServiceTest.java
@@ -1,4 +1,13 @@
+package com.springSecurity.JWT.Services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductServiceTest {
-  
+
+    @Test
+    void createProduct() {
+
+    }
 }
