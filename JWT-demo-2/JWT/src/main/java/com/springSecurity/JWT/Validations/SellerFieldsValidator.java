dfb05e7b9def5d6efbb1f18c4f5cc6657d47b56c
vkisyov25 <ventsislav755@gmail.com>
2025-02-15 21:28:00 +0200

    fixed problem with custom validaions

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Validations/SellerFieldsValidator.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Validations/SellerFieldsValidator.java
@@ -1,34 +1,33 @@
 package com.springSecurity.JWT.Validations;
 
-import com.springSecurity.JWT.Models.User;
+import com.springSecurity.JWT.Models.dtos.CreateUserDto;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class SellerFieldsValidator implements ConstraintValidator<ValidSellerFields, User> {
+public class SellerFieldsValidator implements ConstraintValidator<ValidSellerFields, CreateUserDto> {
 
     @Override
-    public boolean isValid(User user, ConstraintValidatorContext context) {
-        if ("seller".equals(user.getAuthorities())) {
+    public boolean isValid(CreateUserDto createUserDto, ConstraintValidatorContext context) {
+        if ("seller".equals(createUserDto.getAuthorities())) {
             boolean isValid = true;
 
-            if (user.getCompanyName() == null || user.getCompanyName().isBlank()) {
+            if (createUserDto.getCompanyName() == null || createUserDto.getCompanyName().isBlank()) {
                 context.disableDefaultConstraintViolation();
-                context.buildConstraintViolationWithTemplate("Company name is required for sellers")
-                        .addPropertyNode("companyName") // Свързваме грешката с конкретно поле
+                context.buildConstraintViolationWithTemplate("Company name is required for sellers").addPropertyNode("companyName") // Свързваме грешката с конкретно поле
                         .addConstraintViolation();
                 isValid = false;
             }
 
-            if (user.getCompanyEIK() == null || user.getCompanyEIK().isBlank()) {
+            if (createUserDto.getCompanyEIK() == null || createUserDto.getCompanyEIK().isBlank()) {
                 context.disableDefaultConstraintViolation();
-                context.buildConstraintViolationWithTemplate("Company EIK is required for sellers")
-                        .addPropertyNode("companyEIK") // Свързваме грешката с конкретно поле
+                context.buildConstraintViolationWithTemplate("Company EIK is required for sellers").addPropertyNode("companyEIK") // Свързваме грешката с конкретно поле
                         .addConstraintViolation();
                 isValid = false;
             }
 
             return isValid;
         }
+
         return true; // Ако не е seller, пропускаме проверката
     }
 }
