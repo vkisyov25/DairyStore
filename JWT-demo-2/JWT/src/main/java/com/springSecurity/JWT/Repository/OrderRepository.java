528329ed34656a058cc6baef9a0d8705d3505057
vkisyov25 <ventsislav755@gmail.com>
2025-02-19 22:33:23 +0200

    created OrderRepository

---
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.springSecurity.JWT.Repository;
+
+import com.springSecurity.JWT.Models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findTopByUserIdOrderByDateDesc(Long userId);
+}
