3774f892e56a758e33ac1d93f57bc53675e9c20f
vkisyov25 <ventsislav755@gmail.com>
2025-02-25 10:09:58 +0200

    added getOrdersByUserId() method to make manipulations in analyzeUserShoppingBehavior() method

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Repository/OrderRepository.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Repository/OrderRepository.java
@@ -2,11 +2,16 @@ package com.springSecurity.JWT.Repository;
 
 import com.springSecurity.JWT.Models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<Order> findTopByUserIdOrderByDateDesc(Long userId);
+
+    @Query("SELECT order FROM Order order WHERE order.userId =?1")
+    List<Order> getOrdersByUserId(Long user_id);
 }
