7e42731ee1f792ca6c5c0deae181ce552d3d300c
vkisyov25 <ventsislav755@gmail.com>
2025-02-28 14:22:39 +0200

    added findOrderItemsByOrderId() method in OrderItemRepository

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Repository/OrderItemRepository.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Repository/OrderItemRepository.java
@@ -2,10 +2,14 @@ package com.springSecurity.JWT.Repository;
 
 import com.springSecurity.JWT.Models.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
-
+    @Query("SELECT orderItem FROM OrderItem orderItem WHERE orderItem.order.id = ?1")
+    List<OrderItem> findOrderItemsByOrderId(Long orderId);
 }
