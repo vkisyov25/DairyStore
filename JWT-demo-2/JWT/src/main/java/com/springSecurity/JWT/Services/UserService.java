d3e45cf6ca922ce380b1c245b833959aadacc156
vkisyov25 <ventsislav755@gmail.com>
2025-03-03 13:17:37 +0200

    added methods for user information editing

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/UserService.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Services/UserService.java
@@ -1,6 +1,7 @@
 package com.springSecurity.JWT.Services;
 
 import com.springSecurity.JWT.Models.User;
+import com.springSecurity.JWT.Models.dtos.UserInformationDto;
 import com.springSecurity.JWT.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -16,7 +17,6 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-
     public User getUserByUsername() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = null;
@@ -24,5 +24,55 @@ public class UserService {
             user = userRepository.findByUsername(authentication.getName());
         }
         return user;
+    }
+
+    public UserInformationDto getCurrentUserInformation() {
+        User user = getUserByUsername();
+        return UserInformationDto.builder()
+                .username(user.getUsername())
+                .authorities(user.getAuthorities())
+                .name(user.getName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .address(user.getAddress())
+                .companyName(user.getCompanyName())
+                .companyEIK(user.getCompanyEIK())
+                .build();
+    }
+
+    public void updateCurrentUserInformation(UserInformationDto userInformationDto) {
+        User user = getUserByUsername();
+
+        if (userInformationDto.getUsername() != null && !userInformationDto.getUsername().isEmpty()) {
+            user.setUsername(userInformationDto.getUsername());
+        }
+        if (userInformationDto.getName() != null && !userInformationDto.getName().isEmpty()) {
+            user.setName(userInformationDto.getName());
+        }
+        if (userInformationDto.getEmail() != null && !userInformationDto.getEmail().isEmpty()) {
+            user.setEmail(userInformationDto.getEmail());
+        }
+        if (userInformationDto.getPhone() != null && !userInformationDto.getPhone().isEmpty()) {
+            user.setPhone(userInformationDto.getPhone());
+        }
+        if (userInformationDto.getAddress() != null && !userInformationDto.getAddress().isEmpty()) {
+            user.setAddress(userInformationDto.getAddress());
+        }
+        if (userInformationDto.getCompanyName() != null && !userInformationDto.getCompanyName().isEmpty()) {
+            user.setCompanyName(userInformationDto.getCompanyName());
+        }
+        if (userInformationDto.getCompanyEIK() != null && !userInformationDto.getCompanyEIK().isEmpty()) {
+            user.setCompanyEIK(userInformationDto.getCompanyEIK());
+        }
+
+        userRepository.save(user);
+
+    }
+
+    public void deleteCompanyInfoOnTheCurrentUser() {
+        User user = getUserByUsername();
+        user.setCompanyName(null);
+        user.setCompanyEIK(null);
+        userRepository.save(user);
     }
 }
