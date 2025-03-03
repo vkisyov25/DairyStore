46726628cc76d7c223a99147b8533044eab38162
vkisyov25 <ventsislav755@gmail.com>
2025-03-03 13:13:57 +0200

    updated UserController with endpoints for user information editing

--- JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/UserController.java
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Controllers/UserController.java
@@ -1,11 +1,21 @@
 package com.springSecurity.JWT.Controllers;
 
+import com.springSecurity.JWT.Models.dtos.UserInformationDto;
 import com.springSecurity.JWT.Services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -17,9 +27,38 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/allInformation")
+    /*@GetMapping("/allInformation")
     public String getUserByUsername(Model model) {
         model.addAttribute("user", userService.getUserByUsername());
         return "userInformationPage";
+    }*/
+
+    @GetMapping("/allInformation")
+    public ResponseEntity<UserInformationDto> getCurrentUserInformation() {
+        return ResponseEntity.ok(userService.getCurrentUserInformation());
     }
+
+    @PostMapping("/edit")
+    public ResponseEntity<?> editUserInformation(@Valid @RequestBody UserInformationDto userInformationDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // Събиране на всички грешки в списък
+            List<String> errors = bindingResult.getAllErrors()
+                    .stream()
+                    .map(error -> error.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            // Връщане на грешките като JSON
+            return ResponseEntity.badRequest().body(Map.of("errors", errors));
+        }
+
+        userService.updateCurrentUserInformation(userInformationDto);
+        return ResponseEntity.ok(Map.of("message", "User updated successfully"));
+    }
+
+    @PostMapping("/edit")
+    public ResponseEntity<?> deleteCompanyInfo() {
+        userService.deleteCompanyInfoOnTheCurrentUser();
+        return ResponseEntity.ok(Collections.singletonMap("message", "Фирмените данни бяха изтрити."));
+    }
+
 }
