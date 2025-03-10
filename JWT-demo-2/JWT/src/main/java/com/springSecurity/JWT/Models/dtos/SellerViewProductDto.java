11f89090f53e6604af6a7968fb43b0b662bd0994
vkisyov25 <ventsislav755@gmail.com>
2025-03-10 23:25:26 +0200

    created SellerViewProductDto to visualisate all products of each user

---
+++ JWT-demo-2/JWT/src/main/java/com/springSecurity/JWT/Models/dtos/SellerViewProductDto.java
@@ -0,0 +1,18 @@
+package com.springSecurity.JWT.Models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SellerViewProductDto {
+    private String name;
+    private String type;
+    private double weight;
+    private double price;
+    private String description;
+    private double discount;
+    private int quantity;
+}
