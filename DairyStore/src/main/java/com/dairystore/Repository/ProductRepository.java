6383e0025a3d213b271fe078e8b5f5da66df2d24
vkisyov25 <ventsislav755@gmail.com>
2025-03-15 09:59:10 +0200

    created a method findProductsForSale() with custom Query which is return a list from ViewProductDto

--- DairyStore/src/main/java/com/dairystore/Repository/ProductRepository.java
+++ DairyStore/src/main/java/com/dairystore/Repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.dairystore.Repository;
 
 import com.dairystore.Models.Product;
-import com.dairystore.Models.dtos.SellerViewProductDto;
+import com.dairystore.Models.dtos.ViewProductDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -19,9 +19,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("select product from Product product where product.id = ?1")
     Product findProductById(Long id);
 
-    @Query("SELECT new com.dairystore.Models.dtos.SellerViewProductDto(product.id, product.name, product.type, product.weight, product.price, product.description, product.discount, product.quantity) FROM Product product WHERE product.user.username =?1")
-    List<SellerViewProductDto> findBySellerUsername(String username);
+    @Query("SELECT new com.dairystore.Models.dtos.ViewProductDto(product.id, product.name, product.type, product.weight, product.price, product.description, product.discount, product.quantity) FROM Product product WHERE product.user.username =?1")
+    List<ViewProductDto> findBySellerUsername(String username);
 
-    @Query("SELECT new com.dairystore.Models.dtos.SellerViewProductDto(product.id, product.name, product.type, product.weight, product.price, product.description, product.discount, product.quantity) FROM Product product WHERE product.id =?1")
-    SellerViewProductDto findByProductId(Long id);
+    @Query("SELECT new com.dairystore.Models.dtos.ViewProductDto(product.id, product.name, product.type, product.weight, product.price, product.description, product.discount, product.quantity) FROM Product product WHERE product.id =?1")
+    ViewProductDto findByProductId(Long id);
+
+    @Query("SELECT new com.dairystore.Models.dtos.ViewProductDto(product.id, product.name, product.type, product.weight, product.price, product.description, product.discount, product.quantity) FROM Product product")
+    List<ViewProductDto> findProductsForSale();
 }
