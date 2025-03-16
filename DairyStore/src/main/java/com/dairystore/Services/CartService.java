c58a2dabbd01143d08e45d6c8da94f712b7c9142
vkisyov25 <ventsislav755@gmail.com>
2025-03-16 15:57:04 +0200

    created a viewShoppingCart() method, which is return a ShoppingCartDto

--- DairyStore/src/main/java/com/dairystore/Services/CartService.java
+++ DairyStore/src/main/java/com/dairystore/Services/CartService.java
@@ -1,11 +1,16 @@
 package com.dairystore.Services;
 
 import com.dairystore.Models.Cart;
+import com.dairystore.Models.CartItem;
 import com.dairystore.Models.Product;
 import com.dairystore.Models.User;
+import com.dairystore.Models.dtos.ShoppingCartDto;
 import com.dairystore.Repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +28,7 @@ public class CartService {
         }
 
         double totalPrice = 0;
-        if (!user.getUsername().isEmpty()) {
+        if (!user.getCompanyName().isEmpty()) {
             totalPrice = product.getPrice() * quantity - ((product.getPrice() * quantity) * (product.getDiscount() / 100));
             totalPrice = Math.round(totalPrice * 100.0) / 100.0;
         } else {
@@ -42,6 +47,30 @@ public class CartService {
 
         /*product.setQuantity(product.getQuantity() - quantity);
         productService.save(product);*/
+    }
+
+    public List<ShoppingCartDto> viewShoppingCart() {
+        User user = userService.getUserByUsername();
+        List<CartItem> cartItemList = cartItemService.getCartItemsByCart(user.getCart());
+
+        return cartItemList.stream()
+                .map(cartItem -> {
+                    double discount = user.getCompanyName().isEmpty() ? 0 : cartItem.getProduct().getDiscount();
+                    double totalPricePerProduct = (cartItem.getQuantity() * cartItem.getProduct().getPrice()) - ((cartItem.getQuantity() * cartItem.getProduct().getPrice()) * (discount / 100));
+                    totalPricePerProduct = Math.round(totalPricePerProduct * 100.0) / 100.0;
+
+                    return ShoppingCartDto.builder()
+                            .id(cartItem.getId())
+                            .name(cartItem.getProduct().getName())
+                            .type(cartItem.getProduct().getType())
+                            .weight(cartItem.getProduct().getWeight())
+                            .discount(discount)
+                            .price(cartItem.getProduct().getPrice())
+                            .quantity(cartItem.getQuantity())
+                            .totalPricePerProduct(totalPricePerProduct)
+                            .build();
+                })
+                .collect(Collectors.toList());
     }
 
     public Cart getCartByUser(User user) {
