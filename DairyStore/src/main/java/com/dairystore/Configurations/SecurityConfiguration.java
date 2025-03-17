7d87e5955baef3da4442a3b2dea85509dd54968c
vkisyov25 <ventsislav755@gmail.com>
2025-03-17 17:18:02 +0200

    updated SecurityConfiguration for the new enpoint

--- DairyStore/src/main/java/com/dairystore/Configurations/SecurityConfiguration.java
+++ DairyStore/src/main/java/com/dairystore/Configurations/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration {
                                 .requestMatchers("/cart/add").hasAuthority("buyer")
                                 .requestMatchers("/cart/view").hasAuthority("buyer")
                                 .requestMatchers(HttpMethod.DELETE,"/cart/{productId}").hasAuthority("buyer")
+                                .requestMatchers("/order/delivery-companies").hasAuthority("buyer")
                                 .requestMatchers("/order/make").hasAuthority("buyer")
                                 .requestMatchers("/order/latest-order").hasAuthority("buyer")
                                 .requestMatchers("/soldProduct/buyerAnalytics").hasAuthority("buyer")
