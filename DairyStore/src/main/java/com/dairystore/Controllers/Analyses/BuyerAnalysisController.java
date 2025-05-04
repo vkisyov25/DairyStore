a55f731736df5f6efa7bcfa99c78336ee5e26604
vkisyov25 <ventsislav755@gmail.com>
2025-05-04 16:18:01 +0300

    created controller for buyer analysis

---
+++ DairyStore/src/main/java/com/dairystore/Controllers/Analyses/BuyerAnalysisController.java
@@ -0,0 +1,21 @@
+package com.dairystore.Controllers.Analyses;
+
+import com.dairystore.Models.dtos.BuyerAnalyticsDto;
+import com.dairystore.Services.analyses.buyer.BuyerAnalysisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/analysis")
+public class BuyerAnalysisController {
+    private final BuyerAnalysisService buyerAnalysisService;
+    @GetMapping("/buyer")
+    public ResponseEntity<BuyerAnalyticsDto> analyzeUserShoppingBehavior() {
+        BuyerAnalyticsDto analytics = buyerAnalysisService.getBuyerAnalysis();
+        return ResponseEntity.ok(analytics);
+    }
+}
