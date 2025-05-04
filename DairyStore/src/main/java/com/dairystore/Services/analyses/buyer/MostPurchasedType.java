67ec5b8ce2be8947f583b9416e3dceafbc003f82
vkisyov25 <ventsislav755@gmail.com>
2025-05-04 16:38:33 +0300

    created buyer analysis

---
+++ DairyStore/src/main/java/com/dairystore/Services/analyses/buyer/MostPurchasedType.java
@@ -0,0 +1,40 @@
+package com.dairystore.Services.analyses.buyer;
+
+import com.dairystore.Models.OrderItem;
+import com.dairystore.Models.User;
+import com.dairystore.Models.dtos.BuyerAnalyticsDto;
+import com.dairystore.Services.OrderItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class MostPurchasedType implements BuyerAnalysisStrategy {
+    private final OrderItemService orderItemService;
+
+    @Override
+    public void analyze(User user, BuyerAnalyticsDto.BuyerAnalyticsDtoBuilder builder) {
+        List<OrderItem> orderItemList = orderItemService.getOrderItemsByCartId(user.getCart().getId());
+
+        Map<String, Integer> categoryCountMap = new HashMap<>();
+        for (int i = 0; i < orderItemList.size(); i++) {
+            int count = 0;
+            if (categoryCountMap.containsKey(orderItemList.get(i).getType())) {
+                count = categoryCountMap.get(orderItemList.get(i).getType());
+                count += orderItemList.get(i).getQuantity();
+                categoryCountMap.put(orderItemList.get(i).getType(), count);
+            } else {
+                categoryCountMap.put(orderItemList.get(i).getType(), orderItemList.get(i).getQuantity());
+            }
+        }
+        String mostPurchasedType = categoryCountMap.entrySet().stream().max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse("");
+
+        builder.mostPurchasedType(mostPurchasedType);
+    }
+}
