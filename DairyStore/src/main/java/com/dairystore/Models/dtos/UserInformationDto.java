8e79b3708e3d32cc94141aff91f9ad4b93c192f0
vkisyov25 <ventsislav755@gmail.com>
2025-05-10 09:39:36 +0300

    changed the regex on name field and the messages on bulgarian language

--- DairyStore/src/main/java/com/dairystore/Models/dtos/UserInformationDto.java
+++ DairyStore/src/main/java/com/dairystore/Models/dtos/UserInformationDto.java
@@ -11,14 +11,14 @@ import lombok.Setter;
 public class UserInformationDto {
     private String username;
     private String authorities;
-    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Невалиден формат на имейл адреса")
     private String email;
-    @Pattern(regexp = "^[A-z a-z]+$", message = "Name must not contain numbers")
+    @Pattern(regexp = "^([А-Яа-яЁё]+ [А-Яа-яЁё]+|[A-Za-z]+ [A-Za-z]+)$", message = "Името трябва да съдържа само букви")
     private String name;
-    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be exactly 10 digits.")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Телефонният номер трябва да е точно 10 цифри.")
     private String phone;
     private String address;
     private String companyName;
-    @Pattern(regexp = "^[0-9]{9}$", message = "Company EIK must be exactly 9 digits.")
+    @Pattern(regexp = "^[0-9]{9}$", message = "EIK номерът на компанията трябва да е точно 9 цифри.")
     private String companyEIK;
 }
