759ac1ec7ccb43521ef09f88b61a0fd7dda82077
vkisyov25 <ventsislav755@gmail.com>
2025-05-18 12:37:46 +0300

    updated the language to bulgarian

--- DairyStore/src/main/java/com/dairystore/Validations/CompanyEIKValidator.java
+++ DairyStore/src/main/java/com/dairystore/Validations/CompanyEIKValidator.java
@@ -13,7 +13,7 @@ public class CompanyEIKValidator implements ConstraintValidator<CompanyEIK, Crea
         if (createUserDto.getAuthorities().equals("seller")) {
             if (!createUserDto.getCompanyEIK().matches("^[0-9]{9}$")) {
                 context.disableDefaultConstraintViolation();
-                context.buildConstraintViolationWithTemplate("CompanyEIK must be only 9 digits").addPropertyNode("companyEIK").addConstraintViolation();
+                context.buildConstraintViolationWithTemplate("ЕИК-то на фирмата трябва да е точно 9 цифри").addPropertyNode("companyEIK").addConstraintViolation();
                 isValid = false;
             }
 
@@ -22,7 +22,7 @@ public class CompanyEIKValidator implements ConstraintValidator<CompanyEIK, Crea
         if (createUserDto.getAuthorities().equals("buyer") && !createUserDto.getCompanyEIK().isEmpty()) {
             if (!createUserDto.getCompanyEIK().matches("^[0-9]{9}$")) {
                 context.disableDefaultConstraintViolation();
-                context.buildConstraintViolationWithTemplate("CompanyEIK must be only 9 digits").addPropertyNode("companyEIK").addConstraintViolation();
+                context.buildConstraintViolationWithTemplate("ЕИК-то на фирмата трябва да е точно 9 цифри").addPropertyNode("companyEIK").addConstraintViolation();
                 isValid = false;
             }
 
